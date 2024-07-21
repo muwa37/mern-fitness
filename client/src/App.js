be521de7e1236dd0af88da3ffb0f89575fec2f77
muwa37 <yond41m3@icloud.com>
2024-07-21 19:36:35 +0400

    Added Auth page

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,30 @@
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider, styled } from 'styled-components';
+import Auth from './pages/Auth';
+import { lightTheme } from './utils/themes';
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background: ${({ theme }) => theme.bg};
+  color: ${({ theme }) => theme.text_primary};
+  overflow-x: hidden;
+  overflow-y: hidden;
+  transition: all 0.2s ease;
+`;
+
 function App() {
-  return <div className='App'>works</div>;
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <BrowserRouter>
+        <Container>
+          <Auth />
+        </Container>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ client/src/pages/Auth.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div``;
+const Left = styled.div``;
+const Right = styled.div``;
+
+const Auth = () => {
+  return (
+    <Container>
+      <Left>l</Left>
+      <Right>r</Right>
+    </Container>
+  );
+};
+
+export default Auth;

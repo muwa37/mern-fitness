df1727d2aee4dbdff3256e7dd77c5fefe2c5a4b0
muwa37 <yond41m3@icloud.com>
2024-07-21 20:00:51 +0400

    Modified Auth page, added Button and Input components

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,15 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import AuthImage from '../assets/AuthImage.jpg';
+import LogoImage from '../assets/Logo.png';
 
-const Container = styled.div``;
-const Left = styled.div``;
-const Right = styled.div``;
+const Container = styled.div`
+  flex: 1;
+  height: 100%;
+  display: flex;
+  background: ${({ theme }) => theme.bg};
+  @media (max-width: 700px) {
+    flex-direction: column;
+  }
+`;
+
+const Left = styled.div`
+  flex: 1;
+  position: relative;
+  @media (max-width: 700px) {
+    display: none;
+  }
+`;
+
+const Right = styled.div`
+  flex: 1;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  padding: 40px;
+  gap: 16px;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Logo = styled.img`
+  position: absolute;
+  width: 60px;
+  top: 40px;
+  left: 60px;
+  z-index: 10;
+`;
+
+const Image = styled.img`
+  position: relative;
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+`;
+
+const Text = styled.div`
+  font-size: 16px;
+  text-align: center;
+  color: ${({ theme }) => theme.text_secondary};
+  margin-top: 16px;
+  @media (max-width: 400px) {
+    font-size: 14px;
+  }
+`;
+
+const TextButton = styled.span`
+  color: ${({ theme }) => theme.primary};
+  cursor: pointer;
+  transition: all 0.3s ease;
+  font-weight: 600;
+`;
 
 const Auth = () => {
+  const [login, setLogin] = useState(false);
   return (
     <Container>
-      <Left>l</Left>
-      <Right>r</Right>
+      <Left>
+        <Logo src={LogoImage} />
+        <Image src={AuthImage} />
+      </Left>
+      <Right>
+        {!login ? (
+          <>
+            <Text>
+              Don't have an account?
+              <TextButton onClick={() => setLogin(false)}> SignUp</TextButton>
+            </Text>
+          </>
+        ) : (
+          <>
+            <Text>
+              Already have an account?
+              <TextButton onClick={() => setLogin(false)}> SignIn</TextButton>
+            </Text>
+          </>
+        )}
+      </Right>
     </Container>
   );
 };

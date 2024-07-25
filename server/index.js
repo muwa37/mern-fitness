bfa13e1fb2bfc4db8183350add11bb0018834abb
muwa37 <yond41m3@icloud.com>
2024-07-25 22:11:02 +0400

    Modified server

---
+++ server/index.js
@@ -0,0 +1,39 @@
+import cors from 'cors';
+import * as dotenv from 'dotenv';
+import express from 'express';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', async (req, res) => {
+  res.status(200).json({
+    message: 'Hello',
+  });
+});
+
+const connectDB = () => {
+  mongoose.set('strictQuery', true);
+  mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => console.log('Connected mongo'))
+    .catch(err => {
+      console.error('Failed to connect mongo');
+      console.error(err);
+    });
+};
+
+const startServer = async () => {
+  try {
+    connectDB();
+    app.listen(8080, () => console.log('Server started on port 8080'));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();

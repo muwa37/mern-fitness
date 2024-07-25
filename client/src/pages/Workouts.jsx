e7cf75524dedf4b07c6479a813317f541edf5feb
muwa37 <yond41m3@icloud.com>
2024-07-25 21:56:20 +0400

    Added workouts page

--- client/src/pages/Workouts.jsx
+++ client/src/pages/Workouts.jsx
@@ -1,7 +1,110 @@
-import React from 'react';
+import { DateCalendar } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Workout from '../components/cards/Workout';
+
+const Container = styled.div`
+  flex: 1;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 22px 0px;
+  overflow-y: scroll;
+`;
+
+const Wrapper = styled.div`
+  flex: 1;
+  max-width: 1600px;
+  display: flex;
+  gap: 22px;
+  padding: 0px 16px;
+  @media (max-width: 600px) {
+    gap: 12px;
+    flex-direction: column;
+  }
+`;
+
+const Left = styled.div`
+  flex: 0.2;
+  height: fit-content;
+  padding: 18px;
+  border: 1px solid ${({ theme }) => theme.text_primary};
+  border-radius: 14px;
+  box-shadow: 1px 6px 20px 0px ${({ theme }) => theme.primary};
+`;
+
+const Title = styled.div`
+  font-weight: 600;
+  font-size: 16px;
+  color: ${({ theme }) => theme.primary};
+  @media (max-width: 600px) {
+    font-size: 14px;
+  }
+`;
+
+const Right = styled.div`
+  flex: 1;
+`;
+
+const CardWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 20px;
+  margin-bottom: 100px;
+  @media (max-width: 600px) {
+    gap: 12px;
+  }
+`;
+
+const Section = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0px 16px;
+  gap: 22px;
+  padding: 0px 16px;
+  @media (max-width: 600px) {
+    gap: 12px;
+  }
+`;
+
+const SectionTitle = styled.div`
+  font-size: 22px;
+  color: ${({ theme }) => theme.text_primary};
+  font-weight: 500;
+`;
 
 const Workouts = () => {
-  return <div>Workouts</div>;
+  const [date, setDate] = useState('');
+  const [todaysWorkouts, setTodaysWorkouts] = useState([]);
+
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Title>Select Date</Title>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DateCalendar
+              onChange={e => setDate(`${e.$M + 1}/${e.$D}/${e.$y}`)}
+            />
+          </LocalizationProvider>
+        </Left>
+        <Right>
+          <Section>
+            <SectionTitle>Todays Workout</SectionTitle>
+
+            <CardWrapper>
+              {todaysWorkouts.map(workout => (
+                <Workout workout={workout} />
+              ))}
+            </CardWrapper>
+          </Section>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default Workouts;

e8cc168eece6a928e16de2f6a0c2a95f3a8ba66a
muwa37 <yond41m3@icloud.com>
2024-07-29 21:56:35 +0400

    Added user controller, modified error handling

---
+++ server/controllers/User.js
@@ -0,0 +1,36 @@
+import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
+import { createError } from '../error.js';
+import User from '../models/User.js';
+
+dotenv.config();
+
+export const UserRegister = async () => {
+  try {
+    const { email, password, name, img } = req.body;
+
+    const isUserExist = await User.findOne({ email }).exec();
+
+    if (isUserExist) {
+      return next(createError(409, 'email already in use'));
+    }
+
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+      img,
+    });
+    const createdUser = await user.save();
+    const token = jwt.sign({ id: createdUser._id }, process.env.JWT, {
+      expiresIn: '1 year',
+    });
+    return res.status(200).json({ token, user });
+  } catch (error) {
+    return next(error);
+  }
+};
--- server/index.js
+++ server/index.js
@@ -10,6 +10,16 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
 
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || 'Something went wrong';
+  return res.status(status).json({
+    success: false,
+    status,
+    message,
+  });
+});
+
 app.get('/', async (req, res) => {
   res.status(200).json({
     message: 'Hello',
